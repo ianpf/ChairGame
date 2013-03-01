ecc74fc1b8f6f030a6d60d6b4fbbd9815b813ffd
Charles Brensinger <c.p.brensinger@student.liv.ac.uk>
2013-03-01 21:28:01 +0000

    Updated level to instantiate a test chair.

--- ChairGame/src/mygame/Level.java
+++ ChairGame/src/mygame/Level.java
@@ -1,10 +1,16 @@
 package mygame;
 
+import chair.input.XboxController;
+import chair.input.XboxInputListener;
 import com.jme3.asset.AssetManager;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.objects.PhysicsRigidBody;
+import com.jme3.input.InputManager;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import java.util.LinkedList;
 
 /**
@@ -16,17 +22,34 @@ public class Level implements PhysicsCollisionListener {
     PhysicsSpace physicsSpace;
     Node rootNode;
     AssetManager assetManager;
+    InputManager inputManager;
     
  
     //Remove these from PhysicsSpace at the beginning of an update.
     LinkedList<GameObject> killUs;
     
-    public Level(Node root, AssetManager manager){
+    public Level(Node root, AssetManager assets, InputManager input){
+        //We REALLY should implement a singleton...
         rootNode = root;
-        assetManager = manager;
+        assetManager = assets;
+        inputManager = input;
         killUs = new LinkedList<GameObject>();
         
-
+        XboxInputListener controllerListener = new XboxInputListener(input);
+        Spatial chairSpatial = 
+                assetManager.loadModel("Models/Angry Chair/Angry Chair.j3o");
+        PhysicsRigidBody chairBody = new PhysicsRigidBody();
+        
+        OfficeChair chair= new OfficeChair(
+                this, 
+                new Vector3f(0.0f, 0.0f, 0.0f),
+                0.0f, 
+                (XboxController) controllerListener.getInputControllers()[0], 
+                chairSpatial,
+                chairBody);
+        
+        rootNode.attachChild(chairSpatial);
+        physicsSpace.addCollisionObject(chairBody);
     }
     
     
