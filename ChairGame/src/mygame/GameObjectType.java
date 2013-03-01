e70a7408e59927f075d7c86537d6c6b22a1987a7
Charles Brensinger <c.p.brensinger@student.liv.ac.uk>
2013-03-01 20:50:04 +0000

    GameObjectType enum, changes to GameObject, Level
    Furealz this time.

--- ChairGame/src/mygame/GameObjectType.java
+++ ChairGame/src/mygame/GameObjectType.java
@@ -1,13 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame;
 
 /**
  *
- * @author Setup
+ * @author Charlie
  */
 public enum GameObjectType {
-    
+    ACTOR,
+    PROJECTILE,
+    OBSTACLE
 }
