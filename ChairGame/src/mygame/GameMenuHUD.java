c93f518fce72e9d61037005baadbfdf6b9812e05
Deahgib <louis.bennette@gmail.com>
2013-03-02 05:03:07 +0000

    Sorting out stuff for the boiiiiiiiis

--- ChairGame/src/mygame/GameMenuHUD.java
+++ ChairGame/src/mygame/GameMenuHUD.java
@@ -19,19 +19,7 @@ import de.lessvoid.nifty.screen.ScreenController;
 import mygame.Main;
 
 public class GameMenuHUD implements ScreenController 
-{
-    private AudioRenderer audioRenderer;
-    private AssetManager assetManager;
-    private InputManager inputManager; 
-    private ViewPort guiViewPort;
-    
-    public GameMenuHUD (AudioRenderer ar, AssetManager am, InputManager im, ViewPort gvp){
-        audioRenderer = ar;
-        assetManager = am;
-        inputManager = im;
-        guiViewPort = gvp;
-    }
-    
+{   
     Nifty nifty;
 
     public void bind(Nifty nifty, Screen screen) {
