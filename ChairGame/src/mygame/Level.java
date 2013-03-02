76a0d80e53c43c548c386b5a235e1125fabd1880
Charles Brensinger <c.p.brensinger@student.liv.ac.uk>
2013-03-02 06:53:31 +0000

    Differentiation between walls and floor.

--- ChairGame/src/mygame/Level.java
+++ ChairGame/src/mygame/Level.java
@@ -52,7 +52,7 @@ public class Level {
         Geometry g = new Geometry("Box Left", b);
         Material mat = new Material(assetManager, 
                 "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.White);
+        mat.setColor("Color", ColorRGBA.LightGray);
         g.setMaterial(mat);
         Wall wall = new Wall(g, -21.0f, -21.0f, 1.0f, 42.0f);
         this.staticObjects.add(wall);
@@ -85,6 +85,7 @@ public class Level {
         max = new Vector3f(21.0f, 0.0f, 21.0f);
         b = new Box(min, max);
         g = (new Geometry("Box Bottom", b));
+        mat.setColor("Color", ColorRGBA.Gray);
         g.setMaterial(mat);
         wall = new Wall(g, -21.0f, -21.0f, 0.0f, 0.0f);
         this.staticObjects.add(wall);
